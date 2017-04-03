35a4afc4ee2f6f68bc4eb94e7b4110c673346903
Igor Nascimento <igor.nascimento@objectedge.com>
2017-04-03 17:32:36 -0300

    Templating..

---
+++ src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DashboardController extends Controller
+{
+
+  /**
+  * @Route("/", name="Dashboard")
+  */
+  public function indexAction() {
+    
+
+    $content = $this->renderView('dashboard.html');
+    
+    return new Response($content);
+
+  }
+
+}
